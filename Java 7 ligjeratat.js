cd18e7535373c9421979e781ea9c6dc7a28da91b
Ramiz Shefkiu <ramiz.shefkiu@nettxio.local>
2023-11-27 00:53:26 +0100

    New Progress changes

---
+++ Java 7 ligjeratat.js
@@ -0,0 +1,19 @@
+//JAVA 7
+
+//DOM reprezenton dokumentin HTML ne nje peme(tree) logjike
+
+//Dokumenti eshte hapesira qe ruajme(deklarojme) kodin
+
+//HTML perbehet prej HEAD dhe BODY
+
+//Pjeset e Domit:Nyja e dokumentit, N. elementeve, N. atributeve, N. teksteve.
+
+//Selektimi i elemnteve ne HTML: selektimi i nje elementi, i shume elementeve, Traversing.
+
+//Variable eshte lokacion i caktuar ne memorie
+
+//.TEXTcontent ndyrsho contentin ne qat paragraf, ben modifikin ne htlm,css,js. Ndersa innerhtlm bon veq ne HTML
+
+//Metodat e Domit qe kthejne me shume se nje element getelementsbyclassname, bytagname, .
+
+//
