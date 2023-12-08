712bd62734cfaa3e952951dd1f7b740e562a1242
TeosHoxha2004 <hoxhateos08@gmail.com>
2023-12-08 13:24:13 +0100

    First-Page Responsive

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ setInterval(nextSlide, 3000);
 
 
 showSlide(currentIndex);
+
+
+
+
+const navSlider = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-items");
+  const navLinks = document.querySelectorAll(".nav-items li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  }); 
+};
+navSlider();
